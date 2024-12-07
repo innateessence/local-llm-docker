236105a2e5dcc13babf7a5dde0fba982a93e1af7
innateessence <jackofspades707@hotmail.com>
2024-12-07 10:39:33 -0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-docker compose up -d --force-recreate
-docker compose exec -it ollama ollama pull llama3.2
+function is_running(){
+    docker compose logs | grep "Uvicorn running on" &> /dev/null
+}
 
-echo "open-webui may take a few minutes to get ready...."
-echo "navigate to http://localhost:3000 after a few minutes"
-echo "then set your default model to llama3.2"
+function await_running(){
+  try=0
+  max_tries=60
+  echo "Waiting for open-webui to be ready..."
+  while [ $max_tries -gt 0 ]; do
+    if is_running; then
+      echo "open-webui is ready"
+      break
+    fi
+    sleep 1
+    try=$((try+1))
+  done
+}
+
+function Main(){
+  docker compose up -d --force-recreate
+  docker compose exec -it ollama ollama pull llama3.2
+
+  echo "open-webui may take a few minutes to get ready...."
+  echo "navigate to http://localhost:3000 after a few minutes"
+  echo "then set your default model to llama3.2"
+
+  await_running
+}
+
+Main
