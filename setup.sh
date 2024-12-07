c89b8d02d6a646f8a0eeb595ec1170c4ccfdc0b1
innateessence <jackofspades707@hotmail.com>
2024-12-07 10:17:30 -0800

    minor bugfix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose up -d --force-recreate
-docker compose exec -it ollama pull llama3.2
+docker compose exec -it ollama ollama pull llama3.2
 
 echo "open-webui may take a few minutes to get ready...."
 echo "navigate to http://localhost:3000 after a few minutes"
